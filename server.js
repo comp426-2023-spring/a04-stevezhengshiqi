148cbdc8172b7dc437b42f4a326246e861d937a6
stevezhengshiqi <zheng.shi.qi@qq.com>
2023-04-20 17:36:38 -0400

    server: Fix rpsls path

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import minimist from "minimist";
-import { rps, rpsls } from "./lib/repsls.js";
+import { rps, rpsls } from "./lib/rpsls.js";
 
 const app = express();
 const args = minimist(process.argv.slice(2));
